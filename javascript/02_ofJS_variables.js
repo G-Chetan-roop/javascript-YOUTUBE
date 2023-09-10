51443efd73e15437140abc33ff949db42e167c72
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2023-09-10 03:52:42 +0000

    container config done

---
+++ javascript/02_ofJS_variables.js
@@ -0,0 +1,12 @@
+const company="MICrosoft"
+//const is used to keep a particular variable to not change
+let name="chetan"
+var accpass="12345"
+accCity="JAIPUR"
+console.table([company,name, accpass,accCity])
+/*
+var if used it cant keep track of scope
+use only let
+variable that is not defined 
+
+*/
