0c2e2e5f4f2b1efd3233478c04445a4e4ea371d0
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2023-09-10 04:58:45 +0000

    container config done

---
+++ javascript/04_conversion.js
@@ -0,0 +1,15 @@
+let score = "33,"
+console.log(typeof score);
+let valurInNumber=Number(score)
+//Number method converts a var into number
+console.log(typeof valurInNumber);
+console.log(valurInNumber);
+//NaN is used to check whether the given value is number or not
+let a=null
+console.log(typeof a);
+//"33" -> num 33
+//"33abs" -> type num but value NaN
+let num=33
+let stnum=String(num)
+console.log(stnum);
+console.log(typeof stnum)
