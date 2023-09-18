cb4c18cc285cc3d3b5c1aff64a45de590f05672f
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2023-09-18 04:43:53 +0000

    stack and heap

---
+++ javascript/06.js
@@ -0,0 +1,17 @@
+//stack memory and heap memory 
+// stack  primitive and heap non primitive
+let name="chetan"
+let anname=name
+anname="csjc"
+console.log(anname);
+console.log(name);
+let user={
+email:"1@gmail.com",
+upi:"ifjei"
+}
+let userTwo=user
+userTwo.email="fnifn"
+console.log(user.email);
+console.log(userTwo.email)
+//stack --> provides a copy of the variables declared
+// heap --> provides a reference of the variables and the variables ca be changed using a reference
