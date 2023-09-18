2c435a2b7585fc9c6802247330eb31c19f6ecbd1
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2023-09-18 04:24:01 +0000

    datatypes typeof

---
+++ javascript/05_datatypes.js
@@ -0,0 +1,22 @@
+//primitive
+// 7 types string,,number,boolean,null,undefined,symbol- to make value unique,BigInt
+//reference (non primitive)
+// array,objects,functions
+//js JavaScript is
+//  a dynamically typed 
+//  language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program 
+//  as we assign different values to them.
+const id=Symbol("223")
+const anId=Symbol("223")
+console.log(id===anId)
+//symbol is used to uniquely identify the variables
+const heros=["h1","h2","h3"]
+let obj={
+    name:"chetan",
+    age:55,
+
+}
+const myfun=function(){
+console.log("hello world")
+}
+// use typeof to find out the datatype of a variable
