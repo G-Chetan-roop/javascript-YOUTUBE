5a0f6022e5638a04dfb71cf30c5be0a7aca0b4ea
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2023-09-20 09:40:01 +0000

    update 3

--- javascript/03_ofJS_datatypes.js
+++ javascript/03_ofJS_datatypes.js
@@ -2,7 +2,10 @@ let name="chetan"
 let age=20
 let isLoggedIn=false
 //null is a standalone value
-//undefined it is not defined
+//undefined it is undefined
 //type of null is an object
 console.log(typeof null);
 console.log(typeof undefined);
+let p=Number(name)
+console.log(typeof p)
+
