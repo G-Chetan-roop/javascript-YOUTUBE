54f704f4a390b8652f1420981e4a522a33f46624
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2023-09-21 14:19:32 +0000

    update the 8 lecture

---
+++ javascript/08_numbersAndMaths.js
@@ -0,0 +1,35 @@
+// const score=400
+// console.log(score);
+// const balance=new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// /*to fixed method gives the precision to a number*/
+// const otherNumber=1123.8966
+// console.log(otherNumber.toPrecision(3)); 
+// /*toPrecision */ 
+
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+/*it is used for converting the number given to a string that gives commas to clear them */ 
+
+/* maths kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk*/
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(43.656))
+// console.log(Math.ceil(5.5))
+// console.log(Math.floor(3.11));
+// console.log(Math.min(5,55,0,55))
+
+console.log(Math.random())
+//only between the 01 
+console.log(Math.random()*10+1)
+const min=10
+const max=20
+
+
+console.log(Math.floor(Math.random() * (max-min+1)+min))
+//formula to get any random values between two numbers
